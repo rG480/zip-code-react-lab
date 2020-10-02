3608719fc8b4cbbf56d902f9e20423319a1e7ea4
rG480 <55470594+rG480@users.noreply.github.com>
2020-10-02 16:07:14 -0400

    Removed Unessecary Files
    City.js,CityAndSearch.js,ZipSearchField.js were created by me and never used so they are now removed.

--- zip-search/src/City.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-class City extends React.Component {
-    render(){
-    return (<div>This is the City component</div>);
-    }
-  }
-export default City
--- zip-search/src/ZipSearchField.js
+++
@@ -1,4 +0,0 @@
-//import React from 'react'
-//export function ZipSearchField(props) {
-  //  return (<div><p>Zip Code:</p><input type="text" /></div>);
-  //}
